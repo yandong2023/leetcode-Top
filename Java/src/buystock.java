2db79d8b07c15153eca356b0a25b244b4f3f601d
yandong <yandong@meishubao.com>
2021-12-10 10:16:11 +0800

    买股票最大利润

--- Java/src/buystock.java
+++ Java/src/buystock.java
@@ -35,6 +35,9 @@
 class Solution {
     public int maxProfit(int[] prices) {
         //思路取最大值和最小值利润即是差值 且最大值一定是在最小值后面
+        if (prices==null || prices.length ==0){
+            return 0;
+        }
         int max=0;
         int min = prices[0];
         for(int i =0; i<prices.length;i++){
